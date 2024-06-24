4dafeb05d7b5a364842eb7471561d3cf78f2da9c
Nahuel Alvarez <alvareznahue@gmail.com>
2024-06-24 13:25:31 -0300

    mejorando articulos

--- components/ArticleElement.js
+++ components/ArticleElement.js
@@ -37,7 +37,7 @@ class ArticleElement extends HTMLElement {
         }
 
       </style>
-      <a href="/section-pages/articulos.html" class="article-container">
+      <a href="./pages-sections/articulos/sub-articulos/${name}.html" class="article-container">
         <img class="article-img" src="./assets/slider-articulos/${name}.jpg" alt="${name}">
         <h2 class="article-h2">${name}</h2>
       </a>
